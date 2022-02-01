d0a57f3a8c48d84b7ace350183080cef1523f790
Mridul Gupta <mridul.gupta@reece.com.au>
2022-02-01 23:19:23 +1100

    hook up button clicks

--- app/src/main/java/com/mg/navigationplayground/ChooseRecipientFragment.kt
+++ app/src/main/java/com/mg/navigationplayground/ChooseRecipientFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.mg.navigationplayground.databinding.FragmentChooseRecipientBinding
 
 class ChooseRecipientFragment : Fragment() {
+    var navController: NavController? = null
+
+    private lateinit var binding: FragmentChooseRecipientBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
+        binding = FragmentChooseRecipientBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        binding.nextBtn.setOnClickListener {
+            navController!!.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+        }
+
+        binding.cancelBtn.setOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/mg/navigationplayground/SpecifyAmountFragment.kt
+++ app/src/main/java/com/mg/navigationplayground/SpecifyAmountFragment.kt
@@ -1,17 +1,38 @@
 package com.mg.navigationplayground
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.mg.navigationplayground.databinding.FragmentSpecifyAmountBinding
 
 class SpecifyAmountFragment : Fragment() {
+
+    private var navController: NavController? = null
+
+    private lateinit var binding: FragmentSpecifyAmountBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    ): View {
+        binding = FragmentSpecifyAmountBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        binding.sendBtn.setOnClickListener {
+            navController!!.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+        }
+
+        binding.cancelBtn.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
 }
