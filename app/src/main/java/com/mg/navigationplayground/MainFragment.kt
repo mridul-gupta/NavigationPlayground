520106c77628e98b47dade02794da06ba32f2ba1
Mridul Gupta <mridul.gupta@reece.com.au>
2022-02-01 23:05:52 +1100

    hook up main screen

--- app/src/main/java/com/mg/navigationplayground/MainFragment.kt
+++ app/src/main/java/com/mg/navigationplayground/MainFragment.kt
@@ -5,14 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.mg.navigationplayground.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    var navController: NavController? = null
+
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.viewTransactionsBtn.setOnClickListener {
+            navController!!.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+        }
+
+        binding.sendMoneyBtn.setOnClickListener {
+            navController!!.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+        }
+
+        binding.viewBalanceBtn.setOnClickListener {
+            navController!!.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
     }
 
 }
